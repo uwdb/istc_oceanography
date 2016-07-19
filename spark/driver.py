ad9f1445e269f1dfc1a733c13dcb5a361462adf8
root <root@ip-172-31-35-121.us-west-2.compute.internal>
2016-07-19 18:57:27 +0000

    completing the spark pipeline, making k a command line parameter

--- spark/driver.py
+++ spark/driver.py
@@ -4,6 +4,12 @@ from pyspark.sql import SQLContext
 from pyspark.sql import Row
 from pyspark.sql.types import *
 from pyspark.sql.functions import count
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-k', dest='kmercount', type=int)
+args = parser.parse_args()
+k = args.kmercount 
 
 def get_samples(filename):
     samples = []
@@ -11,14 +17,11 @@ def get_samples(filename):
         samples.append(sample.strip()[:-4])
     return samples
 
-k = 11 # Length of k-mers
-samples = get_samples('/root/istc_oceanography/metadata/valid_samples_GA02_filenames.csv')
-master_hostname = open("/root/spark-ec2/masters").read().strip()
+samples = get_samples('/root/istc_oceanography/metadata/valid_samples_GA03_filenames.csv')
 master_url = open("/root/spark-ec2/cluster-url").read().strip()
 context = SparkContext(master_url)
 context.setLogLevel("WARN")
 sqlcontext = SQLContext(context)
-
 
 def extract_kmers(r):
     for i in range(0,len(r.seq)-k+1):
@@ -30,35 +33,10 @@ for sample_name in samples:
                 StructField("id", StringType(), True), \
                 StructField("seq", StringType(), True)])
     sample = sqlcontext.read.format('com.databricks.spark.csv').options(header='true').load(sample_filename, schema=customSchema).repartition(80)
-    #sample.flatMap(extract_kmers).map(Row("kmer")).toDF().groupBy("kmer","SampleID").agg(count("*")).repartition(1).write.format('com.databricks.spark.csv').options(header='true').save('s3n://oceankmers/overlapped/'+sample_name+'.csv')
-    sample.flatMap(extract_kmers).map(Row("kmer")).toDF().groupBy("kmer").agg(count("*")).repartition(1).write.format('com.databricks.spark.csv').options(header='true').save(sample_name+'.csv')
-    #kmers_df.registerTempTable("kmers"+sample_name)
-    #sqlcontext.sql("select kmer, count(*) as count from kmers{sample_name} group by kmer".format(sample_name=sample_name)).repartition(1).write.format('com.databricks.spark.csv').options(header='true').save('s3n://oceankmers/overlapped/'+sample_name+'.csv')
-    #sqlcontext.sql("select kmer, count(*) as count from kmers{sample_name} group by kmer".format(sample_name=sample_name)).registerTempTable(sample_name + '_count')
-    #Uncomment the following to export
-    #sqlcontext.sql("select * from {s}".format(s=sample_name + '_count')).repartition(1).write.format('com.databricks.spark.csv').options(header='true').save('s3n://oceankmers/overlapped/'+sample_name+'.csv')
+    sample = sample.flatMap(extract_kmers).map(Row("kmer")).toDF().groupBy("kmer").agg(count("*"))
+    #Toggle comment the following to export the data
+    #sample.registerTempTable(sample_name + "_count")
+    sample.repartition(1).write.format('com.databricks.spark.csv').options(header='true').save(sample_name+'.csv')
+    #Or this for pushing to s3
+    #sample.repartition(1).write.format('com.databricks.spark.csv').options(header='true').save('s3n://oceankmers/overlapped/'+sample_name+'.csv')
 
-#X_sql = """
-#select a.SampleID as asample, b.SampleID as bsample,
-#       case when a.count < b.count then a.count else b.count end as minv,
-#       a.count as acount, b.count as bcount
-#       from {sample1} a, {sample2} b
-#       where a.kmer = b.kmer
-#""".format(sample1=samples[0]+'_count', sample2=samples[1]+'_count')
-#sqlcontext.sql(X_sql).registerTempTable('X')
-
-#suma_sql = """
-#select sum(a.count) as sum_a from {s1} a
-#""".format(s1=samples[0]+'_count') 
-#sqlcontext.sql(suma_sql).registerTempTable('sumA')
-
-#sumb_sql = """
-#select sum(b.count) as sum_b from {s2} b
-#""".format(s2=samples[1]+'_count') 
-#sqlcontext.sql(sumb_sql).registerTempTable('sumB')
-
-#Y_sql = """
-#select asample, bsample, 1 - 2*sum(minv) / (sum_a+sum_b) from X, sumA, sumB
-#group by asample, bsample, sum_a, sum_b
-#"""
-#sqlcontext.sql(Y_sql).show()
