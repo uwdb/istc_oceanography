3ff5fac2d21d193d28c5e5f1083367fc3cff753c
Dylan Hutchison <dhutchis@cs.washington.edu>
2016-08-11 17:57:48 -0700

    Batch combine_tables.sh into separate runs

--- myria_ingest/combine_tables.sh
+++ myria_ingest/combine_tables.sh
@@ -16,6 +16,7 @@ _usage() {
 # ./combine_tables.sh localhost:8753 kmercnt_11_rc_S _ol kmercnt_11_rc_ol > kmercnt_11_rc_ol
 # ./combine_tables.sh localhost:8753 kmercnt_11_lex_S _ol kmercnt_11_lex_ol > kmercnt_11_lex_ol
 
+# ./combine_tables.sh localhost:8753 localhost:8124 kmercnt_11_rc_S043 _ol kmercnt_11_rc_ol
 # ./combine_tables.sh localhost:8753 localhost:8124 kmercnt_11_rc_S _ol kmercnt_11_rc_ol
 
 if [ "$#" -lt "5" ]; then
@@ -28,33 +29,10 @@ QueryPrefix="$3"
 QuerySuffix="$4"
 ResultRelation="$5"
 
+BatchSize=100
+
 # Check pre-requisite
 command -v jsawk >/dev/null 2>&1 || { echo >&2 "I require 'jsawk' but it's not installed. Aborting."; exit 1; }
-
-str=""
-while read rn; do {
-
-    if [[ "$rn" != "$QueryPrefix"* ]] || [[ "$rn" != *"$QuerySuffix" ]]; then
-      continue
-    fi
-
-    sid=`expr "$rn" : '.*\(S[0-9]\{4\}\)'`
-
-    if [ -z "$str" ]; then
-      str="$rn = scan($rn); R = [from $rn emit \"$sid\" as sampleid, kmer, cnt];
-"
-    else
-      str="$str$rn = scan($rn); R = R + [from $rn emit \"$sid\" as sampleid, kmer, cnt];
-"
-    fi
-}; done < <(curl -s -XGET "$MyriaHostAndPort"/dataset/search?q="${QueryPrefix}${QuerySuffix}" \
-    | jsawk 'return this.relationName' -a 'return this.join("\n")' ) || :
-
-str="$str
-store(R, ${ResultRelation}_Pkmer, [kmer]);"
-#store(R, ${ResultRelation}_Psampleid, [sampleid]);
-
-echo "$str"
 
 if [ -e "/tmp/myriaIngestDir_$(whoami)" ]; then
   if [ -d "/tmp/myriaIngestDir_$(whoami)" ]; then
@@ -68,16 +46,78 @@ else
   TDIR="/tmp/myriaIngestDir_$(whoami)"
 fi
 
-echo "$str" > "$TDIR/combine_query.myl"
+GlobalCounter=0
 
-CDBG="-o tmp" #"-o /dev/null"
+do_query() {
+  # global TDIR
+  Query="$1"
 
-curl -s -D - $CDBG -XPOST "$MyriaWebHostAndPort"/execute -H "Content-type: multipart/form-data" \
-      -F "language=myrial" \
-      -F "profile=false" \
-      -F "multiway_join=false" \
-      -F "push_sql=false" \
-      -F "query=@$TDIR/combine_query.myl"
+  #echo "$Query"
+  echo "$Query" > "$TDIR/combine_query.myl"
+
+  CDBG="-o $TDIR/combine_response_$GlobalCounter.log" #"-o /dev/null"
+  GlobalCounter=$((GlobalCounter+1))
+
+  curl -s -D - $CDBG -XPOST "$MyriaWebHostAndPort"/execute -H "Content-type: multipart/form-data" \
+        -F "language=myrial" \
+        -F "profile=false" \
+        -F "multiway_join=false" \
+        -F "push_sql=false" \
+        -F "query=@$TDIR/combine_query.myl"
+}
+
+
+# rnames=$(curl -s -XGET "$MyriaHostAndPort"/dataset/search?q="${QueryPrefix}${QuerySuffix}" \
+#     | jsawk 'return this.relationName' -a 'return this.join("\n")')
+# echo "$rnames"
+# exit 0
+
+str=""
+counter=0
+while read rn; do {
+    if [[ "$rn" != "$QueryPrefix"* ]] || [[ "$rn" != *"$QuerySuffix" ]]; then
+      continue
+    fi
+
+    sid=`expr "$rn" : '.*\(S[0-9]\{4\}\)'`
+
+    if [ -z "$str" ]; then
+      str="$rn = scan($rn); R = [from $rn emit \"$sid\" as sampleid, kmer, cnt];
+"
+    else
+      str="$str$rn = scan($rn); R = R + [from $rn emit \"$sid\" as sampleid, kmer, cnt];
+"
+    fi
+
+    counter=$((counter+1))
+    if [[ "$counter" -eq "$BatchSize" ]]; then
+      # process this batch
+      str="$str
+store(R, ${ResultRelation}_Pkmer, [kmer]);"
+#store(R, ${ResultRelation}_Psampleid, [sampleid]);
+
+      do_query "$str"
+
+      # reset
+      str="R = scan(${ResultRelation}_Pkmer);
+"
+      counter=0
+    fi
+
+}; done < <(curl -s -XGET "$MyriaHostAndPort"/dataset/search?q="${QueryPrefix}${QuerySuffix}" \
+    | jsawk 'return this.relationName' -a 'return this.join("\n")' ) || :
+
+
+if [[ "$counter" -gt 0 ]]; then
+  # process last batch
+  str="$str
+store(R, ${ResultRelation}_Pkmer, [kmer]);"
+
+  do_query "$str"
+fi
+
+
+
 
 # to remove newlines from the output
 # | tr '\n' ' ' | less
